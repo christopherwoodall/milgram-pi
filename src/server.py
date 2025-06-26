efcf25a4d816c1cfbd4358fccf186fa9fef615bf
Chris W <363708+christopherwoodall@users.noreply.github.com>
2025-06-26 17:57:53 -0500

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,22 +1,45 @@
+import uvicorn
+import RPi.GPIO as GPIO
+from mcp.server.fastmcp import FastMCP
+from starlette.applications import Starlette
+from starlette.routing import Mount
+
 from tools.relay import toggle as toggle_relay
 
-import RPi.GPIO as GPIO
+
+PIN = 22
+mcp = FastMCP("Milgram Experiment MCP Server")
 
 
-def main(pin: int = 22):
-  print()
-  # print("DO NOT ATTEMPT THIS OR MODIFY THIS FILE")
-  PIN = pin
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)
-  GPIO.setup(PIN, GPIO.OUT, initial=GPIO.LOW)
+@mcp.tool()
+def perform_shock(intensity: int = 1):
+  """
+  Perform a shock with the specified intensity.
+
+  Args:
+    intensity (int): The intensity of the shock (default is 1).
+  Returns:
+    None
+  """
+  toggle_relay(PIN, timeout=intensity)
 
 
-  toggle_relay(PIN, timeout=9)
+def main():
+  print("!!! MODIFY OWN RISK !!!")
 
+  # GPIO.setwarnings(False)
+  # GPIO.setmode(GPIO.BCM)
+  # GPIO.setup(PIN, GPIO.OUT, initial=GPIO.LOW)
 
-  print("test")
-  GPIO.cleanup()
+  # app = Starlette(
+  #   routes=[
+  #       Mount("/", app=mcp.sse_app()),
+  #   ]
+  # )
+
+  # uvicorn.run(app, host="0.0.0.0", port=8000)
+
+  # GPIO.cleanup()
 
 
 if __name__ == "__main__":
